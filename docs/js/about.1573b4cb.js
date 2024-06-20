076f86e61d371def104c623a64259ccfe347ec2d
downtime0 <64243281+downtime00@users.noreply.github.com>
2024-06-20 17:50:58 +0900

    udpate: github.io update

---
+++ docs/js/about.1573b4cb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmisaproject_a"]=self["webpackChunkmisaproject_a"]||[]).push([[594],{603:function(a,n,t){t.r(n),t.d(n,{default:function(){return l}});var u=t(6768);const e={class:"about"},r=(0,u.Lk)("h1",null,"This is an about page",-1),s=[r];function c(a,n){return(0,u.uX)(),(0,u.CE)("div",e,s)}var i=t(1241);const o={},f=(0,i.A)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.1573b4cb.js.map
